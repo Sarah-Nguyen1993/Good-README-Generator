6e41e8d3110aa5450b95d76eb603616b65b0229e
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-15 17:43:54 -0500

    pluged in data received from index.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,24 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+## Description 
+${data.description}
+## Table of Content 
+${data.table}
+## Installtion
+${data.installation}
+## Usage
+${data.usage}
+## Credits
+${data.credits}
+## Contributing
+${data.contributing}
+## Test
+${data.test}
+## Questions
+${data.questions}
+## License
+${data.license}          
 `;
 }
 
