a65db9da284a788cc4239617780554a88d236827
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-15 22:05:51 -0500

    modified license choices so theri badge can be made

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ const questions = [
 ];
 
 const licenseChoices =
-["Apche License 2.0",
-"GNU General Public License v3.0",
-"MIT License", 
-'BSD 2-Clause "Simplified License"',
-'BSD 3-Clause "New" or "Revised" License',
-"Boost Software License 1.0", 
-"Creative Commons Zero v1.0 Universal",
-"Eclipse Public License 2.0", 
-"GNU Affero General Public License v3.0",
-"GNU General Public License v2.0",
-"GNU Lesser General Public License v2.1",
-"Mozilla Public License 2.0", 
-"The Unlicense"]
+["Apche--License--2.0",
+"GNU--General--Public--License--v3.0",
+"MIT--License", 
+'BSD--2--Clause--Simplified--License"',
+'BSD--3--Clause--Revised--License"',
+"Boost--Software--License--1.0", 
+"Creative--Commons--Zero--v1.0--Universal",
+"Eclipse--Public--License--2.0", 
+"GNU--Affero--General--Public--License--v3.0",
+"GNU--General--Public--License--v2.0",
+"GNU--Lesser--General--Public--License--v2.1",
+"Mozilla--Public--License--2.0", 
+]
 
 //function to get user's information
 function getUserInfo() {
