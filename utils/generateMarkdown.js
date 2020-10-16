9d32ba04bfc80193fc7e2914a306d33fe564abd8
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-15 22:10:34 -0500

    added code for badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,17 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+![badge](https://img.shields.io/badge/license-${data.license}-green)
 ## Description 
 ${data.description}
 ## Table of Contents 
-${data.table}
+### [Description](#Description)
+### [Installtion](#installtion)
+### [Usage](#Usage)
+### [Credits](#Credits)
+### [Contributing](#Contributing)
+### [Test](#Test)
+### [Questions](#Questions)
 ## Installtion
 ${data.installation}
 ## Usage
@@ -16,8 +23,9 @@ ${data.contributing}
 ## Test
 ${data.test}
 ## Questions
-${data.questions}
-My GitHub profile is here: https://github.com/${github}
+${data.questions}  \n
+My GitHub profile: https://github.com/${data.github} \n
+If you have any further questions, please contact me here: <${data.email}>
 ## License
 ${data.license}          
 `;
